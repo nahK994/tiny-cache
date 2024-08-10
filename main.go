c69b907d7a12fc79a090a9dd04c0fb53e352c26b
nahK994 <nkskl6@gmail.com>
2024-08-10 13:12:05 +0600

    create server package

--- main.go
+++ main.go
@@ -1,91 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"log/slog"
-	"net"
+
+	"github.com/nahK994/ScratchCache/server"
 )
 
-type Config struct {
-	ListenAddress string
-}
-
-type Server struct {
-	Config
-	peers     map[*Peer]bool
-	ln        net.Listener
-	addPeerCh chan *Peer
-	quitCh    chan struct{}
-	msgCh     chan []byte
-}
-
-func NewServer(cfg Config) *Server {
-	return &Server{
-		Config:    cfg,
-		peers:     make(map[*Peer]bool),
-		addPeerCh: make(chan *Peer),
-		quitCh:    make(chan struct{}),
-		msgCh:     make(chan []byte),
-	}
-}
-
-func (s *Server) loop() {
-	for {
-		select {
-		case rawMsg := <-s.msgCh:
-			if err := s.handleRawMessage(rawMsg); err != nil {
-				slog.Error("raw message error", "err", s.ListenAddress)
-			}
-		case <-s.quitCh:
-			return
-		case peer := <-s.addPeerCh:
-			s.peers[peer] = true
-		}
-	}
-}
-
-func (s *Server) acceptLoop() error {
-	for {
-		conn, err := s.ln.Accept()
-		if err != nil {
-			return err
-		}
-		go s.handleConn(conn)
-	}
-}
-
-func (s *Server) Start() error {
-	ln, err := net.Listen("tcp", s.ListenAddress)
-	if err != nil {
-		return err
-	}
-
-	s.ln = ln
-
-	go s.loop()
-
-	slog.Info("server running", "listenAddr", s.ListenAddress)
-
-	return s.acceptLoop()
-}
-
-func (s *Server) handleConn(conn net.Conn) {
-	peer := NewPeer(conn, s.msgCh)
-	s.addPeerCh <- peer
-	slog.Info("new peer connected", "remoteAddr", conn.RemoteAddr())
-	if err := peer.readLoop(); err != nil {
-		slog.Error("peer read error", "err", err, "remoteAddr", conn.RemoteAddr())
-	}
-}
-
-func (s *Server) handleRawMessage(msg []byte) error {
-	fmt.Println(string(msg))
-	return nil
-}
-
 func main() {
-	srv := NewServer(Config{
+	srv := server.NewServer(server.Config{
 		ListenAddress: "127.0.0.1:8000",
 	})
 	log.Fatal(srv.Start())
