37a1de690172886f1874550efa8638eddbdef81e
nahK994 <nkskl6@gmail.com>
2024-09-10 01:14:16 +0600

    create resp serializer

--- pkg/resp/serializer.go
+++ pkg/resp/serializer.go
@@ -1,1 +1,21 @@
 package resp
+
+import (
+	"strconv"
+	"strings"
+)
+
+func Serialize(cmd string) (string, error) {
+	if cmd == "" {
+		return "*0\r\n", nil
+	}
+
+	segments := strings.Split(cmd, " ")
+	serializedCmd := "*" + strconv.Itoa(len(segments)) + "\r\n"
+
+	for _, seg := range segments {
+		serializedCmd += ("$" + strconv.Itoa(len(seg)) + "\r\n" + seg + "\r\n")
+	}
+
+	return serializedCmd, nil
+}
