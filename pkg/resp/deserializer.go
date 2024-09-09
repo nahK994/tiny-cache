29688b2a7a4ad4c134d4ddef04cc18560f0b6306
nahK994 <nkskl6@gmail.com>
2024-09-09 22:33:12 +0600

    create resp deserializer

--- pkg/resp/deserializer.go
+++ pkg/resp/deserializer.go
@@ -1,1 +1,57 @@
 package resp
+
+type segmentLength int
+type nextIndex int
+type segment string
+
+func getSegmentLength(cmd string, startIndex int) (segmentLength, nextIndex) {
+	length := 0
+	startIndex++ // Move past the '$' or '*' character
+	for {
+		if cmd[startIndex] == '\r' {
+			break
+		}
+
+		length = 10*length + int(cmd[startIndex]-48)
+		startIndex++
+	}
+
+	return segmentLength(length), nextIndex(startIndex + 1)
+}
+
+func getSegment(cmd string, startIndex int, segLength int) (segment, nextIndex) {
+	seg := ""
+	startIndex++ // Move past the initial '$'
+	for i := startIndex; i < startIndex+segLength; i++ {
+		seg += string(cmd[i])
+	}
+	return segment(seg), nextIndex(startIndex + segLength + 2) // Skip '\r\n' after segment
+}
+
+func Deserializer(cmd string) ([]string, error) {
+	var segments []string
+	numSegments := 0
+	index := 1 // Skip the initial '*'
+
+	// Read number of segments
+	for {
+		if cmd[index] == '\r' {
+			index += 2 // Move past '\r\n'
+			break
+		}
+		numSegments = 10*numSegments + int(cmd[index]-48)
+		index++
+	}
+
+	// Read each segment based on the given number of segments
+	for numSegments > 0 {
+		length, nextIdx := getSegmentLength(cmd, index)
+		seg, nextIdx := getSegment(cmd, int(nextIdx), int(length))
+
+		index = int(nextIdx)
+		segments = append(segments, string(seg))
+		numSegments--
+	}
+
+	return segments, nil
+}
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 echo "1) Start server"
 echo "2) Start client"
-echo "3) Kill process"
+echo "3) Start playground"
+echo "4) Kill process"
 
 read -p "Type: " cmd
 if [[ $cmd == 1 ]]; then
@@ -8,5 +9,7 @@ if [[ $cmd == 1 ]]; then
 elif [[ $cmd == 2 ]]; then
     go run cmd/client/main.go
 elif [[ $cmd == 3 ]]; then
+    go run cmd/playground/main.go
+elif [[ $cmd == 4 ]]; then
     sudo kill -9 $(sudo lsof -t -i:8000)
 fi
