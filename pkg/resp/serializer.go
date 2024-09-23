282118da76c01b16208d2e854c8d8fdea5ae0815
nahK994 <nkskl6@gmail.com>
2024-09-23 14:13:03 +0600

    refactor Serializer

--- pkg/resp/serializer.go
+++ pkg/resp/serializer.go
@@ -7,76 +7,63 @@ import (
 	"github.com/nahK994/TinyCache/pkg/utils"
 )
 
+type CommandProcessor func([]string) string
+
+var respCmd = utils.GetRESPCommands()
+var commandProcessors = map[string]CommandProcessor{
+	respCmd.SET:      processSET,
+	respCmd.GET:      processGenericCommand,
+	respCmd.EXISTS:   processGenericCommand,
+	respCmd.INCR:     processGenericCommand,
+	respCmd.DECR:     processGenericCommand,
+	respCmd.DEL:      processGenericCommand,
+	respCmd.LPUSH:    processGenericCommand,
+	respCmd.LRANGE:   processGenericCommand,
+	respCmd.LPOP:     processGenericCommand,
+	respCmd.FLUSHALL: processFlushAll,
+	respCmd.PING:     processPing,
+}
+
 func getCommandName(cmd string) string {
-	seg := ""
-	for _, ch := range cmd {
-		if ch == ' ' {
-			break
-		}
-		seg += string(ch)
-	}
+	seg := strings.Split(cmd, " ")[0]
 	return strings.ToUpper(seg)
 }
 
 func getCmdSegments(cmd string) []string {
-	var words []string
-	temp := strings.Split(cmd, " ")
-	for _, ch := range temp {
-		if len(ch) == 0 {
-			continue
-		}
-		words = append(words, ch)
-	}
-	return words
+	return strings.Fields(cmd)
 }
 
 func getRESPformat(segments []string) string {
 	serializedCmd := fmt.Sprintf("*%d\r\n", len(segments))
-
-	serializedCmd += fmt.Sprintf("$%d\r\n%s\r\n", len(segments[0]), strings.ToUpper(segments[0]))
-	for i := 1; i < len(segments); i++ {
-		serializedCmd += fmt.Sprintf("$%d\r\n%s\r\n", len(segments[i]), segments[i])
+	for _, seg := range segments {
+		serializedCmd += fmt.Sprintf("$%d\r\n%s\r\n", len(seg), seg)
 	}
 	return serializedCmd
 }
 
-func processSET(cmd string) string {
-	words := getCmdSegments(cmd)
+func processSET(words []string) string {
 	words = []string{words[0], words[1], strings.Join(words[2:], " ")}
 	return getRESPformat(words)
 }
 
-func processGenericCommand(cmd string) string {
-	words := getCmdSegments(cmd)
+func processGenericCommand(words []string) string {
 	return getRESPformat(words)
 }
 
-func Serialize(rawCmd string) string {
-	respCmd := utils.GetRESPCommands()
+func processFlushAll([]string) string {
+	return "*1\r\n$8\r\nFLUSHALL\r\n"
+}
 
-	switch getCommandName(rawCmd) {
-	case respCmd.SET:
-		return processSET(rawCmd)
-	case respCmd.GET:
-		return processGenericCommand(rawCmd)
-	case respCmd.EXISTS:
-		return processGenericCommand(rawCmd)
-	case respCmd.INCR:
-		return processGenericCommand(rawCmd)
-	case respCmd.DECR:
-		return processGenericCommand(rawCmd)
-	case respCmd.DEL:
-		return processGenericCommand(rawCmd)
-	case respCmd.LPUSH:
-		return processGenericCommand(rawCmd)
-	case respCmd.LRANGE:
-		return processGenericCommand(rawCmd)
-	case respCmd.LPOP:
-		return processGenericCommand(rawCmd)
-	case respCmd.FLUSHALL:
-		return "*1\r\n$8\r\nFLUSHALL\r\n"
-	case respCmd.PING:
-		return "*1\r\n$4\r\nPING\r\n"
+func processPing([]string) string {
+	return "*1\r\n$4\r\nPING\r\n"
+}
+
+func Serialize(rawCmd string) string {
+	words := getCmdSegments(rawCmd)
+	commandName := getCommandName(rawCmd)
+
+	if processor, exists := commandProcessors[commandName]; exists {
+		return processor(words)
 	}
 	return ""
 }
