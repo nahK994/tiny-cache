87958a9cb8b239ad801a8444f4be120ed809fafd
nahK994 <nkskl6@gmail.com>
2024-09-30 13:45:20 +0600

    update folder structure

--- pkg/resp/serializer.go
+++ pkg/resp/serializer.go
@@ -1,10 +1,20 @@
 package resp
 
 import (
+	"fmt"
 	"strings"
 
+	"github.com/nahK994/TinyCache/pkg/cache"
 	"github.com/nahK994/TinyCache/pkg/shared"
 )
+
+func processArray(segments []string) string {
+	response := fmt.Sprintf("*%d\r\n", len(segments))
+	for _, v := range segments {
+		response += fmt.Sprintf("$%d\r\n%s\r\n", len(v), v)
+	}
+	return response
+}
 
 type CommandProcessor func([]string) string
 
@@ -52,4 +62,34 @@ func Serialize(rawCmd string) string {
 		return processor(words)
 	}
 	return ""
+}
+
+func SerializeBool(arg bool) string {
+	if arg {
+		return ":1\r\n"
+	} else {
+		return ":0\r\n"
+	}
+}
+
+func SerializeCacheItem(item cache.CacheData) string {
+	switch item.DataType {
+	case cache.Int:
+		if item.IntData != nil {
+			return fmt.Sprintf(":%d\r\n", *item.IntData)
+		}
+	case cache.String:
+		if item.StrData != nil {
+			return fmt.Sprintf("$%d\r\n%s\r\n", len(*item.StrData), *item.StrData)
+		}
+	case cache.Array:
+		if item.StrList != nil {
+			response := fmt.Sprintf("*%d\r\n", len(item.StrList))
+			for _, v := range item.StrList {
+				response += fmt.Sprintf("$%d\r\n%s\r\n", len(v), v)
+			}
+			return processArray(item.StrList)
+		}
+	}
+	return "$-1\r\n"
 }
